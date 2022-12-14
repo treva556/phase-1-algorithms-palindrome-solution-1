26e066a675171aa1d69bfab0f35924b80fdbfde8
treva556 <marktreva92@gmail.com>
2022-12-14 12:57:00 +0300

    done

--- index.js
+++ index.js
@@ -1,7 +1,90 @@
+  
+
+
+
+// function reverseString(word) {
+//   // TODO: implement string reversing functionality
+//   return word;
+// }
+
+// function isPalindrome(word) {
+//   // Write your algorithm here
+//   // reverse the input string
+//   // if the reversed string is the same as the input
+//   //   return true
+//   // else
+//   //   return false
+
+//   const reversedWord = reverseString(word);
+//   // if the reversed string is the same as the input
+//   if (word === reversedWord) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+
+// }
+
+
+// if (require.main === module) {
+//   // add your own custom tests in here
+//   console.log("Expecting: true");
+//   console.log("=>", isPalindrome("racecar"));
+
+//   console.log("");
+
+//   console.log("Expecting: false");
+//   console.log("=>", isPalindrome("robot"));
+
+//   console.log("");
+
+//   console.log("Expecting: true");
+//   console.log("=>", isPalindrome("abba"));
+
+//   console.log("");
+
+//   console.log("Expecting: true");
+//   console.log("=>", isPalindrome("a"));
+
+//   console.log("");
+
+//   console.log("Expecting: false");
+//   console.log("=>", isPalindrome("hi"));
+
+//   console.log("");
+
+//   console.log("Expecting: false");
+//   console.log("=>", isPalindrome("robot"));
+
+// }
+
+// module.exports = isPalindrome;
+
 function isPalindrome(word) {
   // Write your algorithm here
+  
+}
+function reverseString(word) {
+  // create an array from the input string
+  const wordArray = word.split("");
+  // reverse the array
+  const reversedWordArray = wordArray.reverse();
+  // create a string from the reversed array
+  const reversedWord = reversedWordArray.join("");
+  // return the reversed string
+  return reversedWord;
 }
 
+function isPalindrome(word) {
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // if the reversed string is the same as the input
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
+}
 /* 
   Add your pseudocode here
 */
